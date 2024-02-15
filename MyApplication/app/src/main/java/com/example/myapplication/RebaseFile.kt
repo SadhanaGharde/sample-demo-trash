5324cb6a4bb76e685a9474822dccee05fa0fe24c
Sadhana <sadhana.bhagat5@gmail.com>
2024-02-15 16:03:18 +0530

    commit3  branch-c-rebase rebase commit 3 var

--- MyApplication/app/src/main/java/com/example/myapplication/RebaseFile.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/RebaseFile.kt
@@ -4,4 +4,6 @@ class RebaseFile {
     // file added for rebase operation
     // commit 2 variable added
     val commit2 = "hgkjlklkl"
+    // commit 3 variable
+    val commit3 = "jkjlklkluyyuyiuiyioy"
 }
